e04c3908355c36b7d491dd2a7825ac0795658727
Mitchell de Zylva <mitch.dezylva@gmail.com>
2019-01-10 14:18:09 +1100

    Added Model evaluation code

--- first_ml_python/main.py
+++ first_ml_python/main.py
@@ -74,3 +74,26 @@ X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
 # Test options and evaluation metric
 seed = 7
 scoring = 'accuracy'
+
+# The plots tell us that there are some relationships between models, so we
+# expect a certain level of Linearity
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
